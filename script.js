1e34c9177bbc183824c8d39f00f914c0627bfc77
Tanaya Asalkar <145457627+tanayaasalkar1@users.noreply.github.com>
2026-09-03 20:45:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let counter = document.getElementById('counter');
+let btn = document.getElementById('incrementBtn');
+let count = 0;
+
+btn.addEventListener('click', () => {
+	alert(count); // show the value BEFORE incrementing
+	count++;
+	counter.textContent = count; // update the display after incrementing
+});
